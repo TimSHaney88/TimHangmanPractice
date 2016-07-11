a47de4cefb5a62d213a769c18d9af6c4497230d1
Tim <thaney88@yahoo.com>
2016-07-11 10:21:36 -0700

    added player.cpp

--- Player.cpp
+++ Player.cpp
@@ -9,38 +9,60 @@ Player::Player()
 {
 	int numberOfWrongGuesses = 0;
 	int totalNumberOfGuesses = 0;
-	//vector<char>listCharGuessed = " ";
+	vector<char>listCharGuessed;
 }
 void Player::getNewLetter(char letter, bool isGuessCorrect)
 {
-
+	if (!isGuessCorrect)
+	{
+		++numberOfWrongGuesses;
+	}
+	++totalNumberOfGuesses;
+	listCharGuessed.push_back(letter);
 }
+
 bool Player::isLetter(char letter) 
 {
-
-
+	if ((letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z'))
+	{
+		return true;
+	}
+	else return false;
 }
 int Player::getNumberOfWrongGuesses()
 {
-
+	return numberOfWrongGuesses;
 }
 int Player::getTotalNumberOfGuesses()
 {
-	return 0;
+	return totalNumberOfGuesses;
 }
-vector<char>getListCharGuessed()
+vector<char> Player::getListCharGuessed()
 {
-	return;
+	return listCharGuessed;
 }
 void Player::displayGuessedLetters()
 {
-
+	for (int i = 0; i < listCharGuessed.size(); i++)
+	{
+		cout << listCharGuessed[i] << " ";
+	}
 }
 bool Player::alreadyGuessed(char guessedLetter)
 {
-	return 0;
+	for (int i = 0; i < listCharGuessed.size(); i++)
+	{
+		if (guessedLetter == listCharGuessed[i])
+		{
+			return true;
+		}
+		else return false;
+	}
+	
 }
 void Player::reset()
 {
-
+	numberOfWrongGuesses = 0;
+	totalNumberOfGuesses = 0;
+	listCharGuessed.clear();
 }
